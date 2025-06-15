577f28eef7005e96cee261f6e9a9bcc8aab36f29
Praveen swami <praveenswami8967@gmail.com>
2025-06-15 12:21:27 +0530

    final app complete

--- backhand/frontend/src/context/SocketContext.jsx
+++ backhand/frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketProvider = ({ children }) => {
   useEffect(() => {
   if (authuser) {
    
-    const newSocket = io("http://localhost:5002", {
+    const newSocket = io("https://chat-app-c1fj.onrender.com", {
       query: { userId: authuser.user._id },
       withCredentials: true,
     });
