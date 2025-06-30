ca731cbf6bb9aed9d4ee43fd4670cb25c066f172
Praveen swami <praveenswami8967@gmail.com>
2025-06-30 18:46:40 +0530

    all final app

--- backhand/SocketIO/server.js
+++ backhand/SocketIO/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chat-app-c1fj.onrender.com"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   },
 });
--- backhand/frontend/src/context/SocketContext.jsx
+++ backhand/frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketProvider = ({ children }) => {
   useEffect(() => {
   if (authuser) {
    
-    const newSocket = io("https://chat-app-c1fj.onrender.com", {
+    const newSocket = io("http://localhost:5002", {
       query: { userId: authuser.user._id },
       withCredentials: true,
     });
